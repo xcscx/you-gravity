6062d944c3235bbe9aaa2d34bbcd34202546f159
ITegg <1362067461@qq.com>
2025-04-18 15:07:38 +0800

    1. 项目创建 - 提供gitignore文件

---
+++ src/main/java/com/itegg/yougravitybackend/YouGravityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itegg.yougravitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouGravityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouGravityBackendApplication.class, args);
+    }
+
+}
