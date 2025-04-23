25846c2128b274e15e5faf0617de633abfca960b
ITegg <1362067461@qq.com>
2025-04-23 09:39:48 +0800

    1. 调整项目扫描mapper包 2. 调整mapper映射字段

--- src/main/java/com/itegg/yougravitybackend/YouGravityBackendApplication.java
+++ src/main/java/com/itegg/yougravitybackend/YouGravityBackendApplication.java
@@ -1,9 +1,13 @@
 package com.itegg.yougravitybackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.itegg.yougravitybackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class YouGravityBackendApplication {
 
     public static void main(String[] args) {
