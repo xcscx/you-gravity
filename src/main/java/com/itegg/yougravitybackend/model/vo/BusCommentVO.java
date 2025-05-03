4e740f45dfddababcc2ecf30346d9543a655a3ee
ITegg <1362067461@qq.com>
2025-05-03 20:28:28 +0800

    评论模块vo类提供

---
+++ src/main/java/com/itegg/yougravitybackend/model/vo/BusCommentVO.java
@@ -0,0 +1,41 @@
+package com.itegg.yougravitybackend.model.vo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+
+public class BusCommentVO {
+
+    /**
+     * 地点id
+     */
+    private Long locationId;
+
+    /**
+     * 评论信息
+     */
+    private String message;
+
+    /**
+     * 点赞数量
+     */
+    private Integer likeCount;
+
+    /**
+     * 回复的评论，默认为0
+     */
+    private Long parentId;
+
+    /**
+     * 创建人
+     */
+    private Long createBy;
+
+    /**
+     * ---- 关联字段 ----
+     */
+
+    /**
+     * 是否点赞
+     */
+    private boolean isLike;
+
+}
